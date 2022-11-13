8d60418c328b00072bdc211a94f754cac91e5d00
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2022-11-13 16:15:59 +0100

    I added date on JS and tried to clean the code in all three files

--- weather.js
+++ weather.js
@@ -1,20 +1,46 @@
 function formatDate(date) {
-         
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
 
-  let months= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
-  let currentYear = date.getFullYear();
-  let currentMonth = months[date.getMonth()];
+
   let currentDay = days[date.getDay()];
+  let currentMonth = months[date.getMonth()];
+  let currentYear = date.getFullYear();
   let currentDate = date.getDate();
-  
-  
-  let formattedDate =
-     `Today's date is ${currentDay}, ${currentMonth},${currentDate}, ${currentYear}`;
 
-   return formattedDate;
-  }
- let date = new Date("2022/10/11");
- console.log(formatDate(date));
-  
+  let formattedDate =
+    `
+    Today's date is 
+    ${currentDay} 
+    ${currentMonth}
+    ${currentDate},
+    ${currentYear}
+    `;
+  return formattedDate;
+}
+
+let date = new Date();
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(date);
