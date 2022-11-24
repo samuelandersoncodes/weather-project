6fcb4835c60e05c3203ec16f94062f4284ed2919
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2022-11-24 16:44:29 +0100

    I made someone run the same code. and had a status of 200. I cleaned up the code.
    I will take another break and start the whole JS again

--- weather.js
+++ weather.js
@@ -61,55 +61,17 @@ let time = new Date();
 let timeElement = document.querySelector("#time")
 timeElement.innerHTML = formatTime(date);
 
-function showWeather(event) {
-  
-  // let apiKey = "2494e01f1e94ad8c5106665c9c6e14ae"
+// function showWeather(response) {
+// }
+
+function tellWeather(event) {
+  event.preventDefault();
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=2494e01f1e94ad8c5106665c9c6e14ae&units=metric`; 
-  // axios.get(apiUrl).then(showWeather);
   console.log(apiUrl);
 } 
 
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 16;
-}
-let celsiusLink = document.querySelector("#celsius");
-celsiusLink.addEventListener("click", convertToCelsius);
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 46;
-}
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-// function showWeather(response) {
-    // let temperature = Math.round(response.data.main.temp);
-    // let city = response.data.name;
-    // let weatherReport = `It is ${temperature}°C in ${city}`;
-    // let h1 = document.querySelector("h1");
-    // h1.innerHTML = weatherReport;
-    // console.log(response);
-  // }
+// axios.get(apiUrl).then(showWeather);
 
 
-
-
-
-// 
-
-
-// let apiKey = "dd9de2db37b425827a3d32ecdc9508d4";
-// let units = "metric";
-// let city = "city";
-// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=${apiKey}`
-// axios.
-
-// function tellTemperature(response) {
-
-// }
 
 
