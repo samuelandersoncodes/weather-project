65549d2978fd529b694e8ac4708ea03f6101a6ce
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2022-11-26 00:13:10 +0100

    All bugs are solved now
    I finally used fetch and had status 200.
    I also removed the favicon link from the head of my HTML

--- weather.js
+++ weather.js
@@ -61,16 +61,10 @@ let time = new Date();
 let timeElement = document.querySelector("#time")
 timeElement.innerHTML = formatTime(date);
 
-function showWeather(response) {
-  console.log(response);
-}
-function showApiElement(event) {
-  event.preventDefault();
-  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=2494e01f1e94ad8c5106665c9c6e14ae&units=metric"; 
-  axios.get(apiUrl).then(showWeather);
-}
 
-
+fetch('https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=2494e01f1e94ad8c5106665c9c6e14ae&units=metric')
+  .then(res => res.json())
+  .then(data => console.log(data.main.temp))
 
 
 
