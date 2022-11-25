8d307f31e01138d6730e1c3f705bc346093bf51e
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2022-11-25 15:38:35 +0100

    Still on debugging.
    the UA error still exist

--- weather.js
+++ weather.js
@@ -65,10 +65,11 @@ function showWeather(response) {
   console.log(response);
 }
 function showApiElement(event) {
-  
+  event.preventDefault();
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=2494e01f1e94ad8c5106665c9c6e14ae&units=metric"; 
   axios.get(apiUrl).then(showWeather);
 }
+
 
 
 
