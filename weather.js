5884be0f0d9eb5e090f554494feb1f55d4c77944
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2022-12-10 20:14:19 +0100

    I added description to the weather elements

--- weather.js
+++ weather.js
@@ -61,26 +61,42 @@ let time = new Date();
 let timeElement = document.querySelector("#time")
 timeElement.innerHTML = formatTime(date);
 
+
 function displayWeather(response) {
-console.log(response.data)
+  let cityElement = document.getElementById("city");
   let temp = document.getElementById("temperature");
   let humidity = document.getElementById("humidity");
   let wind = document.getElementById("wind");
   let pressure = document.getElementById("pressure");
-  let dateHolder = document.getElementById("date");
-
+  let description = document.getElementById("description");
+  // let dateHolder = document.getElementById("date");
+  
+  cityElement.innerHTML = response.data.name;
   temp.innerHTML = Math.round(response.data.main.temp);
   humidity.innerHTML = Math.round(response.data.main.humidity);
   wind.innerHTML = Math.round(response.data.wind.speed);
   pressure.innerHTML = Math.round(response.data.main.pressure);
-  dateHolder.innerHTML = formatDate(response.data.dt * 1000);
-
+  description.innerHTML = (response.data.weather[0].description);
+  // dateHolder.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let city = "Berlin";
-const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-axios.get(apiUrl).then(displayWeather);
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.getElementById("city-input");
+  search(cityInput.value);
+}
+let cityInput = document.getElementById("city-input");
+console.log(cityInput.value.search())
+
+search("Berlin");
+let form = document.getElementById("form");
+form.addEventListener("sumbit", handleSubmit);
 
 
 
